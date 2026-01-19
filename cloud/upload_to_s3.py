0c0ca291ab7b7f07e430ebc9efc990db47422bf3
Awamba Alex <124752677+xender237@users.noreply.github.com>
2026-01-19 22:34:14 +0100

    Add script to upload video to Amazon S3
    This script uploads a processed video to Amazon S3.

--- cloud/upload_to_s3.py
+++ cloud/upload_to_s3.py
@@ -1,1 +1,18 @@
+"""
+Upload vers Amazon S3
+---------------------
+Envoie la vidéo traitée vers le stockage cloud.
+"""
+
+import boto3
+
+s3 = boto3.client("s3")
+
+BUCKET_NAME = "videop-bucket"
+FILE_PATH = "/data/downscaled/video_downscaled.mp4"
+S3_KEY = "videos/video.mp4"
+
+s3.upload_file(FILE_PATH, BUCKET_NAME, S3_KEY)
+
+print("✅ Upload vers S3 terminé")
 
